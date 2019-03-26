a42e84823019d45b0babbfec2befcb020c700d37
Adrien Chesneau <achesne@a-018818.ups.u-psud.fr>
2019-03-26 16:07:16 +0100

    Collisions fonctionelles

--- map.js
+++ map.js
@@ -1,8 +1,16 @@
 let canvas = document.getElementById("cvn");
 let context = canvas.getContext("2d");
 
-let player1 = {src: "player1.png", x:100, y:250, length:33, height:22, speed:8};
+let player1 = {src: "player1.png", x:10, y:10, width:15, height:22, speed:8};
 let block_list = [];
+let wall1 = {src:"", x:0, y:0, width:800, height:0};
+let wall2 = {src:"", x:800, y:0, width:0, height:600};
+let wall3 = {src:"", x:0, y:600, width:800, height:0};
+let wall4 = {src:"", x:0, y:0, width:0, height:600};
+block_list.push(wall1);
+block_list.push(wall2);
+block_list.push(wall3);
+block_list.push(wall4);
 
 let json = '{ "lvl1" : "000000000000000001010101101010100000000000000000010101011010101000000000000000000101010110101010010101011010101000000000000000000101010110101010000000000000000001010101101010100000000000000000" }';
 
@@ -12,7 +20,7 @@ let parser= function(pathname){
    let lvl = data.lvl1;
    for(let i=0; i<lvl.length; i++){
         if(lvl[i]==1){
-            block_list.push({x:(i%16)*50,y:Math.trunc(i/16)*50});
+            block_list.push({src:"block.png", x:(i%16)*50,y:Math.trunc(i/16)*50, width:50, height:50});
         }
    }
 }   
@@ -23,60 +31,66 @@ let draw = function (){
     context.drawImage(background,0,0);
     block_list.forEach(e => {
         block = new Image();
-        block.src = "block.png";
-        context.drawImage(block,e.x,e.y);
+        block.src = e.src;
+        context.drawImage(block,e.x,e.y,e.width, e.height);
     });
     player = new Image();
     player.src = player1.src;
-    //console.log(player1.x +" "+player1.y);
-    context.drawImage(player,player1.x,player1.y,33,22);
+    console.log(player1.x +" "+player1.y);
+    context.drawImage(player,player1.x,player1.y,player1.width,player1.height);
+}
+
+let collision = function(player, elem_list){
+    let pos = {x:-1, y:-1, width:-1, height:-1};
+    // let bool = false;
+    elem_list.forEach(elem => {
+        if(! (elem.x >= player.x + player.width || elem.x + elem.width <= player.x || elem.y >= player.y + player.height || elem.y + elem.height <= player.y) ) {
+                // console.log("Collision : " + elem.x + " " + elem.y);
+                // console.log("cpt : " + cpt);
+                // bool = true;
+                pos = {x:elem.x, y:elem.y, width:elem.width, height:elem.height};
+        }
+    });
+    return pos;
 }
 
 let keyboard = function (e) {
+    let pos = {x:-1, y:-1, width:-1, height:-1};
     switch (e.keyCode){
         case 90:
-            if(player1.y>=player1.speed){
-                let bool = true;
-                block_list.forEach(e => {
-                    if(((e.x<player1.x && player1.x<e.x+50) || (e.x<player1.x+22 && player1.x+22<e.x+50)) && (player1.y>=e.y+50 && e.y+50>player1.y-2)){
-                        bool = false;
-                    }
-                });
-                if(bool){player1.y= player1.y-player1.speed;}
+            player1.y= player1.y-player1.speed; 
+            pos = collision(player1, block_list);
+            // console.log(pos);
+            // console.log(pos.x + " " + pos.y);
+            if(pos.x !== -1 || pos.y !== -1){
+                player1.y = pos.y+pos.height;
             }
             break;
         case 81:
-            if(player1.x>=player1.speed){
-                let bool = true;
-                block_list.forEach(e => {
-                    if(((e.y<player1.y && player1.y<e.y+50) || (e.y<player1.y+33 && player1.y+33<e.x+50)) && (player1.x>=e.x+50 && e.x+50>player1.x-2)){
-                        bool = false;
-                    }
-                });
-                if(bool){player1.x= player1.x-player1.speed;}
+            player1.x= player1.x-player1.speed;
+            pos = collision(player1, block_list);
+            // console.log(pos);
+            // console.log(pos.x + " " + pos.y);
+            if(pos.x !== -1 || pos.y !== -1){
+                player1.x = pos.x+pos.width; 
             }
             break;
         case 83:
-            if(player1.y+player.height<600-player1.speed){
-                let bool = true;
-                block_list.forEach(e => {
-                    if(((e.x<player1.x && player1.x<e.x+50) || (e.x<player1.x+22 && player1.x+22<e.x+50)) && (player1.y+33<=e.y && e.y<player1.y+35)){
-                        bool = false;
-                    }
-                });
-                if(bool){player1.y= player1.y+player1.speed;}
+            player1.y= player1.y+player1.speed;
+            pos = collision(player1, block_list);
+            // console.log(pos);
+            // console.log(pos.x + " " + pos.y);
+            if(pos.x !== -1 || pos.y !== -1){
+                player1.y = pos.y-player.height; 
             }
-            else { player1.y = 600-player1.height; }
             break;
         case 68:
-            if(player1.x+player1.length<=800-player1.speed){
-                let bool = true;
-                block_list.forEach(e => {
-                    if(((e.y<player1.y && player1.y<e.y+50) || (e.y<player1.y+33 && player1.y+33<e.x+50)) && (player1.x+22<=e.x && e.x<player1.x+24)){
-                        bool = false;
-                    }
-                });
-                if(bool){player1.x= player1.x+player1.speed;}
+            player1.x= player1.x+player1.speed;
+            pos = collision(player1, block_list);
+            // console.log(pos);
+            // console.log(pos.x + " " + pos.y);
+            if(pos.x !== -1 || pos.y !== -1){
+                player1.x= pos.x-player.width; 
             }
             break;
     }
